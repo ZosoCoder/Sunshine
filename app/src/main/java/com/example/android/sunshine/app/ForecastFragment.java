30aa944026ea0d1e147e7496c24fd7d337e8e60c
ZosoCoder <zosolzrd@gmail.com>
2015-06-22 12:46:35 -0700

    Add code to pull location from SharePreferences
    - onOptionsItemSelects() in ForecastFragment.java

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -2,9 +2,11 @@ package com.example.android.sunshine.app;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -59,7 +61,12 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("92832");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String zip = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+
+            Log.v(LOG_TAG, "ZipCode: " + zip);
+            weatherTask.execute(zip);
             return true;
         }
         return super.onOptionsItemSelected(item);
